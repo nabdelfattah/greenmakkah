400d2e548227eb94481c81d8c376a134f1d764ba
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2024-08-02 01:41:31 +0300

    feat: Complete HTML and CSS for MobileApp section (excluding JS and responsive styles)

--- src/sections/MobileApp.jsx
+++ src/sections/MobileApp.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import styles from "./MobileApp.module.scss";
+import phoneWebp from "../assets/images/phone.webp";
+import phonePng from "../assets/images/phone.png";
+import googlePlay from "../assets/icons/google-play.png";
 
 export default function MobileApp() {
-  return <div></div>;
+  return (
+    <section className={styles.sectionApp}>
+      <div className={styles.grid}>
+        <picture className={styles.imgWrapper}>
+          <source srcSet={phoneWebp} type="image/webp" />
+          <img src={phonePng} className={styles.img} />
+        </picture>
+        <div className={styles.textWrapper}>
+          <header className={styles.header}>
+            <h2 className={styles.heading}>تطبيق أخضر مكة</h2>
+            <hr className={styles.headingLine} />
+          </header>
+          <p>
+            يوفر مستكشف أخضر مكة علي الجوال ﻟﻠﻤﻮاﻃﻨﻴﻦ واﻟﻤﺴﺘﺨﺪﻣﻴﻦ البحث عن
+            الأماكن الخضراء المتاحة في قاعدة البيانات الجغرافية
+          </p>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.green.Makkah"
+            aria-label="Download from App store"
+          >
+            <img src={googlePlay} alt="google play store" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
