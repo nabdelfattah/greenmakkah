2fef327946fea7a4cf3d42946131b2990ab9d4ae
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2024-08-05 22:29:11 +0300

    feat: Complete implementation of MobileApp component

--- src/sections/MobileApp.jsx
+++ src/sections/MobileApp.jsx
@@ -2,16 +2,25 @@ import styles from "./MobileApp.module.scss";
 import phoneWebp from "../assets/images/phone.webp";
 import phonePng from "../assets/images/phone.png";
 import googlePlay from "../assets/icons/google-play.png";
+import { useElementOnScreen } from "../useElementOnScreen";
 
 export default function MobileApp() {
+  const [isPicVisible, observedPicRef] = useElementOnScreen();
+  const [isTxtVisible, observedTxtRef] = useElementOnScreen();
   return (
     <section className={styles.sectionApp}>
       <div className={styles.grid}>
-        <picture className={styles.imgWrapper}>
+        <picture
+          className={`${styles.imgWrapper} ${isPicVisible && "show"}`}
+          ref={observedPicRef}
+        >
           <source srcSet={phoneWebp} type="image/webp" />
           <img src={phonePng} className={styles.img} />
         </picture>
-        <div className={styles.textWrapper}>
+        <div
+          className={`${styles.textWrapper} ${isTxtVisible && "show"}`}
+          ref={observedTxtRef}
+        >
           <header className={styles.header}>
             <h2 className={styles.heading}>تطبيق أخضر مكة</h2>
             <hr className={styles.headingLine} />
--- src/useElementOnScreen.js
+++ src/useElementOnScreen.js
@@ -5,17 +5,20 @@ export function useElementOnScreen(options, state){
   const [isVisible, setIsVisible] = useState(false)
   const observedElRef = useRef(null)
 
-  function callback (entries){
+  function callback (entries, observer){
     const [entry] = entries;
-    setIsVisible(entry.isIntersecting)
+    if(entry.isIntersecting) {
+      setIsVisible(true)
+      observer.unobserve(entry.target) // Unobserve the element after it becomes visible
+    }
   }
-  
+
   useEffect(() => {
     // create observer obj
     const observer = new IntersectionObserver(callback, options)
     // observe target element
     if (observedElRef.current) {
-      observer.observe(observedElRef.current)
+      observer.observe(observedElRef.current) 
     }
     // cleanup
     return ()=>{if(observedElRef.current) observer.unobserve(observedElRef.current)}
@@ -23,3 +26,6 @@ export function useElementOnScreen(options, state){
 
   return [isVisible, observedElRef]
 }
+
+
+
